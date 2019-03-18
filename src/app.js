a3fed9535e9f00709f32dc74777d1c89b9d72e38
yonihomi <yoni.homi@fiverr.com>
2019-03-18 22:14:52 +0200

    herkou

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs = require('hbs')
 
 const app = express()
 
+
+const port = process.env.PORT || 3000
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -26,6 +28,6 @@ app.get('/about', (req, res) => {
 })
 
 
-app.listen(5000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+port)
 })
