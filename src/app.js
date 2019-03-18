c3332a4d6654117400a238118268f67ff681a8f3
yonihomi <yoni.homi@fiverr.com>
2019-03-18 20:52:03 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -1,4 +1,38 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
 
-console.log(path.join(__dirname,'../public'))
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+console.log('@@ viewsPath:'+viewsPath)
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(5000, () => {
+    console.log('Server is up on port 3000.')
+})
